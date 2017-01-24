ffb382226a94468c5bf293619508817d92b285ec
Takkasila Saichol <takkasila@outlook.com>
2017-01-24 10:46:39 +0700

    Base shader

---
+++ fragment_shader.glsl
@@ -0,0 +1,4 @@
+void main()
+{
+    gl_FragColor = vec4(gl_FragCoord.x/800.0, gl_FragCoord.y/600.0, 1.0, 1.0);
+}
--- webgl.js
+++ webgl.js
@@ -9,7 +9,6 @@ class Timer {
         this.counter = this.startTime
         this.msecPerFrame = interval
     }
-
     isReady()
     {
         this.counter = Date.now()
@@ -18,7 +17,6 @@ class Timer {
         else
             return false
     }
-
     tick()
     {
         this.counter = this.markTime = Date.now()
@@ -27,21 +25,16 @@ class Timer {
 
 function webGLStart() {
     initWebGL()
-    gl.clearColor(1.0, 0.0, 0.0, 1.0)
-    gl.clearDepth(1.0)
-    gl.enable(gl.DEPTH_TEST)
-    gl.depthFunc(gl.LEQUAL)
-
-    while(true)
-    {
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-        console.log(Date.now())
-    }
+    initShaders()
+    initBuffer()
+    drawScene()
 }
 
-function initWebGL() {
+function initWebGL()
+{
     canvas = document.getElementById("glCanvas")
-    try {
+    try
+    {
         gl = canvas.getContext("webgl") ||
             canvas.getContext("experimental-webgl") ||
             canvas.getContext("moz-webgl") ||
@@ -49,9 +42,89 @@ function initWebGL() {
         gl.viewportWidth = canvas.width;
         gl.viewportHeight = canvas.height;
         var extensions = gl.getSupportedExtensions()
-        console.log(gl)
-        console.log(extensions)
-    } catch (error) {
-        alert("Browser does not support WebGL");
+        // console.log(gl)
+        // console.log(extensions)
+        gl.clearColor(1.0, 0.0, 0.0, 1.0)
+        gl.clearDepth(1.0)
+        gl.enable(gl.DEPTH_TEST)
+        gl.depthFunc(gl.LEQUAL)
     }
+    catch (error)
+    {
+        alert("Browser does not support WebGL")
+    }
+}
+
+function initShaders()
+{
+    var vertexShader = getShader(gl, "shader-vs")
+    var fragmentShader = getShader(gl, "shader-fs")
+
+    shaderProgram = gl.createProgram()
+    gl.attachShader(shaderProgram, vertexShader)
+    gl.attachShader(shaderProgram, fragmentShader)
+    gl.linkProgram(shaderProgram)
+
+    if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS))
+        console.log("Unable to initialize the shader program: " + gl.getProgramInfoLog(shaderProgram))
+
+    gl.useProgram(shaderProgram)
+
+    vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "pos")
+    gl.enableVertexAttribArray(vertexPositionAttribute)
+}
+
+function getShader(gl, id, type)
+{
+    var shaderScript, theSource, currentChild, shader
+
+    shaderScript = document.getElementById(id)
+    if(!shaderScript)
+        return null
+
+    theSource = shaderScript.text
+    if(!type)
+    {
+        if(shaderScript.type == "x-shader/x-fragment")
+            type = gl.FRAGMENT_SHADER
+        else if (shaderScript.type == "x-shader/x-vertex")
+            type = gl.VERTEX_SHADER
+        else
+            return null
+    }
+
+    shader = gl.createShader(type)
+    gl.shaderSource(shader, theSource)
+    gl.compileShader(shader)
+
+    if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+    {
+        console.log("Error compiling shader: " + gl.getShaderInfoLog(shader))
+        gl.deleteShader(shader)
+        return null
+    }
+
+    return shader
+}
+
+function initBuffer()
+{
+    var vertex_buffer_data =
+    [
+        -1.0, -1.0, 0.0
+        , 1.0, -1.0, 0.0
+        , 0.0, 1.0, 0.0
+    ]
+    vertexBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertex_buffer_data), gl.STATIC_DRAW)
+}
+
+function drawScene()
+{
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+    gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0)
+    gl.drawArrays(gl.TRIANGLES, 0, 3)
 }
